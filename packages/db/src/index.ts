01eb272b9d878ac69f9723696b808f942ee6164a
Jung Hwan Kim <118165671+itsjustcozyboy@users.noreply.github.com>
2026-06-12 02:54:15 +0000

    auto: index.ts 변경 저장

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/client';
+import { PrismaClient } from '../../apps/web/src/generated/prisma';
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
