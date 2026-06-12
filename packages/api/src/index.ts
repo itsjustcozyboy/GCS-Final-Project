02d125bd967be82467d189b5d6c2e29870c7f9bb
Jung Hwan Kim <118165671+itsjustcozyboy@users.noreply.github.com>
2026-06-12 13:18:46 +0000

    auto: index.ts 변경 저장

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -5,6 +5,7 @@ export * from './routers/connection';
 export * from './routers/question';
 export * from './routers/answer';
 export * from './routers/reaction';
+export * from './routers/message';
 export * from './routers/book';
 export * from './routers/tracking';
 export * from './services/question-engine';
