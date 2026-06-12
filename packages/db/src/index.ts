baa984c389ae0b9cb658b22fd00dd1656358cbcf
Jung Hwan Kim <118165671+itsjustcozyboy@users.noreply.github.com>
2026-06-12 02:54:21 +0000

    auto: index.ts 변경 저장

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -10,4 +10,4 @@ export const prisma =
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
-export * from '../generated/client';
+export * from '../../apps/web/src/generated/prisma';
